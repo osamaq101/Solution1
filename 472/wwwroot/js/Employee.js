a9b23b6e673eee27007c0656e8454a55a72dd350
Osama <usamasight@gmail.com>
2024-10-05 20:46:23 -0500

    lab5

---
+++ 472/wwwroot/js/Employee.js
@@ -0,0 +1,22 @@
+﻿class Employee {
+
+    #fullName;
+    #salary = 0;
+
+    static #counter = 0;
+
+    constructor(name, salary, dob) {
+        this.#fullName = name;
+        this.#salary = salary;
+        this.dob = dob;
+        Employee.#incCounter++;
+    }
+
+    static #incCounter() {
+        Employee.#counter++;
+    }
+
+
+
+
+}
