8bd64d2ecdc645729f5985b720bf11c5f6cdf977
Osama <usamasight@gmail.com>
2024-10-06 22:11:51 -0500

    fixed quiz question

--- 472/wwwroot/js/lab5.js
+++ 472/wwwroot/js/lab5.js
@@ -1,1 +1,63 @@
 ﻿
+class Student {
+    constructor(studentId) {
+        this.studentId = studentId;
+        this.answers = []; 
+    }
+
+   
+    addAnswer(question) {
+        this.answers.push(question); 
+    }
+}
+
+class Question {
+    constructor(qid, answer) {
+        this.qid = qid; 
+        this.answer = answer; 
+    }
+
+    
+    checkAnswer(answer) {
+        return this.answer === answer; 
+    }
+}
+
+
+class Quiz {
+    constructor(questions, students) {
+    
+        this.questions = new Map();
+        questions.forEach((question) => {
+            this.questions.set(question.qid, question.answer);
+        });
+        this.students = students; 
+    }
+
+  
+    scoreStudentBySid(sid) {
+        
+        const student = this.students.find((student) => student.studentId === sid);
+        if (!student) return 0; 
+
+       
+        let score = 0;
+        student.answers.forEach((answer) => {
+            const correctAnswer = this.questions.get(answer.qid); 
+            if (answer.checkAnswer(correctAnswer)) {
+                score++; 
+            }
+        });
+        return score;
+    }
+
+ 
+    getAverageScore() {
+
+        const totalScore = this.students.reduce((total, student) => {
+            return total + this.scoreStudentBySid(student.studentId);
+        }, 0);
+    
+        return totalScore / this.students.length;
+    }
+}
