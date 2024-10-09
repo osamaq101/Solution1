58391f944b26aa820baa666a75abe8545bfcc634
Osama <usamasight@gmail.com>
2024-10-08 20:10:36 -0500

    lab7

---
+++ 472/wwwroot/js/lab7t.js
@@ -0,0 +1,79 @@
+﻿
+//Q1
+class Meditation {
+    constructor(duration) {
+        this.duration = duration;
+    }
+
+    start() {
+        let count = this.duration;
+        const countdown = setInterval(() => {
+            if (count > 0) {
+                console.log(count);
+                count--;
+            } else {
+                clearInterval(countdown);
+                console.log("Jay Guru Dev");
+            }
+        }, 1000);
+    }
+}
+
+// Usage example:
+const morning_meditation = new Meditation(5);
+morning_meditation.start();
+console.log(`Start meditation`);
+
+//Q2
+
+
+    
+
+async function isPrime(number)
+{
+
+    return   new Promise((resolved, reject) => {
+        for (let i = 2, s = Math.sqrt(n); i <= s; i++) {
+            if (n % i === 0) {
+                reject({ prime: false });
+            }
+        }
+        resolved({ prime: true });
+
+
+    });
+   
+}
+
+
+function test() {
+    alert("start");
+//console.log('start');
+    (async () => {
+        const result = await isPrime(17);
+        alert(result); 
+    })();
+
+
+    alert("end");
+   // console.log('end');
+
+}
+//q3
+(async () => {
+    try {
+        
+        const response = await fetch('https://dummyjson.com/recipes');
+        if (!response.ok) throw new Error(`HTTP Error: ${response.status}`);
+
+      
+        const data = await response.json();
+
+    
+        const recipes = data.recipes;
+        console.log("Recipe Names:");
+        recipes.forEach(recipe => console.log(recipe.name));
+    } catch (error) {
+        console.error("Error fetching recipes:", error);
+    }
+})();
---
+++ 472/wwwroot/js/test.js
@@ -0,0 +1,55 @@
+﻿
+function Student(studentId)
+{
+   
+        this.studentId = studentId;
+        answers = [];
+  
+
+    this.addAnswer=function (question)
+    {
+        this.answers.push(question);
+    }
+
+}
+
+function Question(qid, answer)
+{
+    this.id = id;
+    this.answer = answer;
+
+    this.checkAnswer = function (answer) {
+        return this.answer === answer;
+    }
+}
+
+function Quiz(questions, students)
+{
+
+    this.question = new Map(questions.Map((question) => [question.id, question.answer]));
+    this.students = students;
+
+
+    this.scoreStudentBySid = function (sid)
+    {
+        const student = this.students.find(x => x.id === sid);
+
+        let score = 0;
+        Students.answers.foreach(x => {
+            const correctAnswer = this.questions.get(answer.qid);
+            if (correctAnswer === x.answer) {
+                score++;
+
+            }
+        }
+
+        );
+        return score;
+    }
+    this.getAverageScore = function ()
+    {
+        const totalScore = this.students.reduce((sum, student) sum + this.scoreStudentBySid(student.sid), 0);
+        return totalScore / this.students.length;
+    }
+
+}
